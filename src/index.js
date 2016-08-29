b2769157e994bba043d6280aaddbc4418eb66fdb
Jon-Hirsch <JonMH99@gmail.com>
2016-08-28 20:11:56 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import Counter from './components/Counter';
-import StateExample from './components/StateExample';
 
 ReactDOM.render((<div>
-                  <Counter/>
+                  <App/>
                 </div>
-              ), document.getElementById('example1'));
+              ), document.getElementById('app'));
 
-ReactDOM.render((<div>
-                  <StateExample/>
-                </div>
-              ), document.getElementById('example2'));
